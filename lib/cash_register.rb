1cfc8dfbac00356e2ffe5fec256bb7de909c42d6
Brandon Dusch <brandondusch@gmail.com>
2020-03-11 02:06:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,6 +37,6 @@ class CashRegister
 end
 
 new_register = CashRegister.new
-      new_register.add_item("eggs", 1.99)
-      new_register.add_item("tomato", 1.76, 3)
+new_register.add_item("eggs", 1.99)
+new_register.add_item("tomato", 1.76, 3)
 binding.pry
